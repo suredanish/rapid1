969e20ce8fd590052392e6935031afae8a3d10a5
Danish Mushtaq <danish.mushtaq@gsat.us>
2025-02-04 19:02:18 +0530

    sign in page

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express');
 const prisma = require('./prisma/db');
 var port = 3000
 var app = express()
+var cors = require('cors')
 app.use(express.json())
+var jwt = require('jsonwebtoken');
+app.use(cors())
 app.get('/helloworld', (req, res) => {
     return res.send("hello world");
 })
@@ -30,17 +33,35 @@ app.post('/signin', async(req, res) => {
     const userInDb = await prisma.user.findFirst({where:{username: user.username}})
     if(userInDb){
         if(userInDb.password == hashPass(user.password)){
-            return res.send("this is the token")
+            delete userInDb.password // userInDb.password = undefined
+            token = getJwt(userInDb)
+            return res.send({token: token})
         }
     }
     return res.status(401).send("Username of Password is not valid")
     // select * from user where username = ${user.username}
 })
+
+app.get('/hello-user', async(req, res) => {
+    return res.send("Hello User")
+})
 app.listen(port, () => console.log("listing on " + port))
+
+
+
 
 function hashPass(password){
     const hash = crypto.createHmac('md5', secret) // Using HMAC with MD5 and a secret key
         .update(password) // Update with the data to hash
         .digest('hex'); // Output in hexadecimal format
     return hash;
+}
+
+function getJwt(user) {
+    var token = jwt.sign({
+        // learn unix time
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+        data: user
+      }, secret);
+      return token;
 }
